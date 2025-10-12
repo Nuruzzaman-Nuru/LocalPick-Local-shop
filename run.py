299fb9c9cc3709f731b8cbc333b9591f4ab3f87c
Nuruzzaman-Nuru <nnuru8805@gmail.com>
2025-10-12 23:58:07 +0600

    new

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 import os
 from ecommerce import create_app
 
-# Flask app তৈরি
 app = create_app(os.getenv('FLASK_CONFIG', 'development'))
 
-# Entry point
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
